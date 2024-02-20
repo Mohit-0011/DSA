26d56a7f8a1ed454b07d433bace685b39fe9305d
MOHIT <130787509+Mohit-0011@users.noreply.github.com>
2024-02-20 22:26:29 +0530

    Add files via upload

---
+++ Java_Basics/If_else.java
@@ -0,0 +1,126 @@
+// package Assignment_Practice_Question;
+import java.util.*;
+
+public class If_else {
+    
+//     Question I : Write a Java program to get a number from the user and print whether it is positive or negative
+
+//     public static void main (String Args[]){
+//         Scanner si = new Scanner(System.in);
+//         int x = si.nextInt();
+
+//         if ( x<0 ){
+//             System.out.println("The Number is Negative");
+//         }
+//         else if ( x>0 ){
+//             System.out.println("The Number is Positive");
+//         }
+//         else{
+//             System.out.println("The Number is ZERO");
+//         }
+//     }
+
+// Question 2 : It prints You have a fever if your temperature is above 100 and otherwise prints You don't have a fever
+
+
+// public static void main (String Args[]){
+//     double temp = 103.5;
+
+//     if (temp > 100){
+//         System.out.println("You have Fever");
+//     }
+//     else{
+//         System.out.println("You don't have Fever");
+//     }
+// }
+
+
+// Question 3 : Write a Java and print day of week name using switch case,
+
+// public static void main (String Args[]){
+//     Scanner si = new Scanner (System.in);
+//      System.out.println("Which Day?");
+//     int day = si.nextInt();
+
+//     switch(day){ 
+//         case 1 :
+//             System.out.println("Today is Monday");
+//             break;
+//         case 2 :{
+//             System.out.println("Today is Tuesday");
+//         break;
+//         }
+//         case 3 :{
+//             System.out.println("Today is Wednesday");
+//         break;
+//         }
+//         case 4 :{
+//             System.out.println("Today is Thur");
+//         break;
+//         }
+//         case 5 :{
+//             System.out.println("Today is Fri");
+//         break;
+//         }
+//         case 6 :{
+//             System.out.println("Today is Sat");
+//         break;
+//         }
+//         case 7 :{
+//             System.out.println("Today is Sun");
+//         break;
+//         }
+//         default:
+//             System.out.println("Wrong Input");
+//     }
+// }
+
+
+// Question 5 : Write a Java program that takes a year from the user and print whether that year is a leap year or not.
+// Hint
+// if completely divisible from 100 not a leap year but if divisible by both 100 and 400 then leap year
+// Otherwise if simply completely divisible by 4 then leap year
+
+
+
+public static void main (String Args[]){
+    Scanner si = new Scanner (System.in);
+     System.out.println("Which Year Do you want to check for leap Year?");
+    int year = si.nextInt();
+
+
+
+    boolean x = (year % 4) == 0;
+    boolean y = (year % 100) != 0;
+    boolean z = ((year % 100) == 0 && (year % 400) == 0)  ;
+
+    if ( x && (y || z) ){
+        System.out.println("It is a leap Year");
+    }
+    // if else ( y = true ){
+    //      System.out.println("Not leap Year");
+    // }
+    else{
+        System.out.println("Not leap Year");
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+
+
+
