eb4c77cea9760b4ee379133bb92f980c0c56c07f
MOHIT <130787509+Mohit-0011@users.noreply.github.com>
2024-02-20 22:44:16 +0530

    Update If_else.java

--- Java_Basics/If_else.java
+++ Java_Basics/If_else.java
@@ -83,27 +83,46 @@ public class If_else {
 
 
 
-public static void main (String Args[]){
-    Scanner si = new Scanner (System.in);
-     System.out.println("Which Year Do you want to check for leap Year?");
-    int year = si.nextInt();
+// public static void main (String Args[]){
+//     Scanner si = new Scanner (System.in);
+//      System.out.println("Which Year Do you want to check for leap Year?");
+//     int year = si.nextInt();
 
 
 
-    boolean x = (year % 4) == 0;
-    boolean y = (year % 100) != 0;
-    boolean z = ((year % 100) == 0 && (year % 400) == 0)  ;
+//     boolean x = (year % 4) == 0;
+//     boolean y = (year % 100) != 0;
+//     boolean z = ((year % 100) == 0 && (year % 400) == 0)  ;
 
-    if ( x && (y || z) ){
-        System.out.println("It is a leap Year");
-    }
+//     if ( x && (y || z) ){
+//         System.out.println("It is a leap Year");
+//     }
     // if else ( y = true ){
     //      System.out.println("Not leap Year");
     // }
-    else{
-        System.out.println("Not leap Year");
+    // else{
+    //     System.out.println("Not leap Year");
         
-    }
+    // }
+
+    // Question 1 Odd Number or Even number 
+    // public static void main (String Args[]){
+    //     Scanner sc = new Scanner (System.in );
+    //     System.out.println("Input Number : ");
+    //     int num = sc.nextInt();
+
+    //     if ( num == 0){
+    //         System.out.println("The Number is Zero");
+    //     }
+        
+    //     else if ( num % 2 == 0){
+    //         System.out.println("The Number is Even ");
+    //     }
+    //     else{
+    //         System.out.println("The Number is Odd ");
+    //     }
+        
+    // }
 
 
 
